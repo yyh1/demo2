ad3310220fc9865b1f26480c38b38d7b5ddd4b96
yyh1 <1372902989@qq.com>
2019-03-13 11:12:03 +0800

    helloword的项目

---
+++ demo/src/demo1/AllTests.java
@@ -0,0 +1,11 @@
+package demo1;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ sizeyunsuanTest.class, zhengchuTest.class })
+public class AllTests {
+
+}
